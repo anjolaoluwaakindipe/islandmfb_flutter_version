efa3fe073eef56cb5666f5f0fa9e908027e4c698
Anjolaoluwa Akindipe <anjyakindipe@gmail.com>
2022-02-23 15:54:14 +0100

    added navigation to login

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,11 +1,10 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
-import 'package:islandmfb_flutter_version/components/shared/app_button.dart';
+
 import 'package:islandmfb_flutter_version/utilities/colors.dart';
 
-class name extends StatelessWidget {
-  const name({Key? key}) : super(key: key);
+class LoginPage extends StatelessWidget {
+  const LoginPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -16,8 +15,8 @@ class name extends StatelessWidget {
       body: Column(
         children: <Widget>[
           SvgPicture.asset("../../logo.svg"),
-          Text("Use your email address, phone number or account number sd your login id"),
-
+          const Text(
+              "Use your email address, phone number or account number sd your login id"),
         ],
       ),
     );
