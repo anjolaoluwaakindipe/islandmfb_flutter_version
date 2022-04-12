5b95595429faa340f12f12b72a17f32cb4a76873
Damisi Babalola <dami600bab@gmail.com>
2022-04-12 14:01:20 +0100

    messages

--- lib/requests/airtime_request.dart
+++ lib/requests/airtime_request.dart
@@ -14,6 +14,41 @@ String sha512hasher(String token, String email, String username) {
   return sha512Result.toString();
 }
 
+Future<Map> makeAirtimePurchase(String phoneNumber, String id) {
+  String hash =
+      sha512hasher(devtoken, "anjyakindipe@gmail.com", "Anjolaoluwaakindipe");
+  Map<String, dynamic> body = {
+    "username": devAirtimeUsername,
+    "hash": hash,
+    "request": [
+      {
+        "product": id,
+        "phone": phoneNumber,
+        "amount": 50,
+        "country": devAirtimeCountry
+      },
+    ]
+  };
+  String urlString = "https://estoresms.com/network_processing/v/2";
+
+  return http
+      .post(
+    Uri.parse(urlString),
+    body: json.encode(body),
+  )
+      .then((value) {
+    if (value.statusCode == 200) {
+      return {"success": true, "data": json.decode(value.body)};
+    }
+    return {"success": false, "data": json.decode(value.body)};
+  }).catchError((_) {
+    return {
+      "success": false,
+      "data": "An error occured please try again later"
+    };
+  });
+}
+
 Future<Map> getProductList(String phoneNumber) {
   String hash =
       sha512hasher(devtoken, "anjyakindipe@gmail.com", "Anjolaoluwaakindipe");
@@ -26,7 +61,12 @@ Future<Map> getProductList(String phoneNumber) {
 
   String urlString = "https://estoresms.com/network_list/v/2";
 
-  return http.post(Uri.parse(urlString), body: json.encode(body),).then((value) {
+  return http
+      .post(
+    Uri.parse(urlString),
+    body: json.encode(body),
+  )
+      .then((value) {
     if (value.statusCode == 200) {
       return {"success": true, "data": json.decode(value.body)};
     }
@@ -40,5 +80,5 @@ Future<Map> getProductList(String phoneNumber) {
 }
 
 void main() async {
-  print(await getProductList("+22347030444529"));
+  print(await makeAirtimePurchase("+2347030444529", "MFIN-5-OR"));
 }
